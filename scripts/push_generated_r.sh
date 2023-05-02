80d9ed90102f44d7cdf50f1b9cc9dc8c7ba2aa6d
Kunitsyn Oleg <oleg.kunitsyn@genestack.com>
2023-05-02 20:15:11 +0200

    update way for install R packages

--- scripts/push_generated_r.sh
+++ scripts/push_generated_r.sh
@@ -12,12 +12,14 @@ done
 # Push R SDK's
 cd generated/r
 
+
+# https://help.sonatype.com/repomanager3/nexus-repository-administration/formats/r-repositories#RRepositories-UploadbyHTTPPOST
 for archive in $(find -type f -name "*.tar.gz" -printf '%P\n') ; do
     if echo ${ODM_OPENAPI_VERSION} | grep -Exq "^([a-zA-Z0-9]+(.)?){3}$"; then
       curl --user "${NEXUS_USER}:${NEXUS_PASSWORD}" \
-         --upload-file "$archive" "${R_REGISTRY_RELEASES}/genestack/odm/api-sdk/$archive"
+         --upload-file "$archive" "${R_REGISTRY_RELEASES}/src/contrib/$archive"
     else
       curl --user "${NEXUS_USER}:${NEXUS_PASSWORD}" \
-         --upload-file "$archive" "${R_REGISTRY_SNAPSHOTS}/genestack/odm/api-sdk/$archive"
+         --upload-file "$archive" "${R_REGISTRY_SNAPSHOTS}/src/contrib/$archive"
     fi
 done
