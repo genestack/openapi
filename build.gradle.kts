db01686fe888162a505f6a014e8e4f106a7c4a80
Kunitsyn Oleg <oleg.kunitsyn@genestack.com>
2024-01-18 15:17:58 +0100

    [ODM-11001] Create skeletom for definition merger

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,19 @@
 import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
+import com.genestack.openapi.MergeDefinitions
 import kotlin.io.path.listDirectoryEntries
 import kotlin.io.path.name
 import kotlin.io.path.Path as KotlinPath
 
 plugins {
     alias(libs.plugins.openapi.generator) apply true
-    `custom-greetings`
 }
 
 val openApiVersion: String = System.getenv("ODM_OPENAPI_VERSION") ?: "1.0.0"
-val openApiSpecPath = "$rootDir/openapi/v1"
-val fileNameList = KotlinPath(openApiSpecPath).listDirectoryEntries("*.yaml")
+val openapiDefinitionsDirectory = "$rootDir/openapi/v1"
+val openapiDefinitionsMergedFileName = "merged.yaml"
+val fileNameList = KotlinPath(openapiDefinitionsDirectory)
+    .listDirectoryEntries("*.yaml")
+    .filterNot { it.name == openapiDefinitionsMergedFileName }
 val tasksList = fileNameList
         .map { it.name.replace(".yaml", "") }
 
@@ -29,12 +32,15 @@ fun String.kebabToCamelCase(): String {
     return this.replace(pattern) { it.groupValues[2].uppercase() }
 }
 
+val openapiDefinitionsSourceFileList = fileNameList.map {
+    layout.projectDirectory.file("${openapiDefinitionsDirectory}/${it}")
+}
 
 tasks {
     for (task in tasksList) {
         register(task + "Python", GenerateTask::class) {
             generatorName.set("python")
-            inputSpec.set("$rootDir/openapi/v1/$task.yaml")
+            inputSpec.set("${openapiDefinitionsDirectory}/${task}.yaml")
             outputDir.set("$rootDir/generated/python/${task.camelToKebabCase()}")
             packageName.set(task.camelToSnakeCase())
             gitUserId.set("genestack")
@@ -46,7 +52,7 @@ tasks {
         }
         register(task + "R", GenerateTask::class) {
             generatorName.set("r")
-            inputSpec.set("$rootDir/openapi/v1/$task.yaml")
+            inputSpec.set("${openapiDefinitionsDirectory}/${task}.yaml")
             outputDir.set("$rootDir/generated/r/${task.kebabToCamelCase()}")
             packageName.set(task.kebabToCamelCase())
             gitUserId.set("genestack")
@@ -56,6 +62,11 @@ tasks {
             )
             skipValidateSpec = true
         }
+    }
+
+    register("mergeDefinitions", MergeDefinitions::class) {
+        inputFiles = openapiDefinitionsSourceFileList
+        outputFile = layout.projectDirectory.file("${openapiDefinitionsDirectory}/${openapiDefinitionsMergedFileName}")
     }
 
     val generateAllApiClients by registering(GradleBuild::class) {
