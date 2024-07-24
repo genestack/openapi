df40c3a22ed0257f4b7efc0fe87045adcb70e9e5
Viacheslav <viacheslav.pivovarov@genestack.com>
2024-07-24 15:09:21 +0400

    ODM-11697: Enable search in internal nexus + set snapshot version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,23 @@
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven {
+            url = java.net.URI.create(System.getenv("MAVEN_REGISTRY_SNAPSHOTS"))
+            credentials {
+                username = System.getenv("NEXUS_USER")
+                password = System.getenv("NEXUS_PASSWORD")
+            }
+        }
+        maven {
+            url = java.net.URI.create(System.getenv("MAVEN_REGISTRY_RELEASES"))
+            credentials {
+                username = System.getenv("NEXUS_USER")
+                password = System.getenv("NEXUS_PASSWORD")
+            }
+        }
+    }
+}
+
 rootProject.name = "openapi"
