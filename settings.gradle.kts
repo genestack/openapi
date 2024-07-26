bbfa4fde946eb61de18828ee1450654076a09544
Viacheslav <viacheslav.pivovarov@genestack.com>
2024-07-26 16:51:14 +0400

    ODM-11697: use uri

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,14 +4,14 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
         maven {
-            url = java.net.URI.create(System.getenv("MAVEN_REGISTRY_SNAPSHOTS"))
+            url = uri(System.getenv("MAVEN_REGISTRY_SNAPSHOTS"))
             credentials {
                 username = System.getenv("NEXUS_USER")
                 password = System.getenv("NEXUS_PASSWORD")
             }
         }
         maven {
-            url = java.net.URI.create(System.getenv("MAVEN_REGISTRY_RELEASES"))
+            url = uri(System.getenv("MAVEN_REGISTRY_RELEASES"))
             credentials {
                 username = System.getenv("NEXUS_USER")
                 password = System.getenv("NEXUS_PASSWORD")
