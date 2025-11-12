2f23345591276ae1472dc1767d38e93244770c3f
Mikhail Smazhevsky <mikhail.smazhevsky@genestack.com>
2025-11-12 17:45:27 +0100

    ODM-12847: Differential expression for cell analysis (#492)
    * ODM-12847: cell analysis: diff expression

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,7 +60,9 @@ tasks {
             // Workaround for https://github.com/OpenAPITools/openapi-generator/issues/21619
             // The second version asks for license, which we can't provide due to unavailability of
             // "licenseName" and "licenseUrl" fields in the specification for python generator.
-            "poetry1" to "true"
+            "poetry1" to "true",
+            // All float fields accept "NaN", "Infinity", "-Infinity" values
+            "mapNumberTo" to "float"
 //            "disallowAdditionalPropertiesIfNotPresent" to "true"
         )
     }
