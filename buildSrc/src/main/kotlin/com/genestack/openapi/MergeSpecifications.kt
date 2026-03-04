a33156f1f14657114c4d4297325de70bad85c7d8
Kunitsyn Oleg <oleg.kunitsyn@genestack.com>
2026-03-04 03:43:56 +0100

    [ODM-13126] Add json file

--- buildSrc/src/main/kotlin/com/genestack/openapi/MergeSpecifications.kt
+++ buildSrc/src/main/kotlin/com/genestack/openapi/MergeSpecifications.kt
@@ -4,11 +4,14 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.file.RegularFile
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.ListProperty
+import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.InputFiles
 
 import tools.jackson.databind.ObjectMapper
+import tools.jackson.databind.SerializationFeature
+import tools.jackson.databind.json.JsonMapper
 import tools.jackson.dataformat.yaml.YAMLFactory
 
 
@@ -20,14 +23,25 @@ abstract class MergeSpecifications : DefaultTask() {
     @get:OutputFile
     abstract val outputFile: RegularFileProperty
 
+    @get:OutputFile
+    @get:Optional
+    abstract val outputFileJson: RegularFileProperty
+
     @TaskAction
     fun merge() {
-        val objectMapper = ObjectMapper(YAMLFactory())
+        val yamlMapper = ObjectMapper(YAMLFactory())
+        val jsonMapper = JsonMapper.builder().enable(SerializationFeature.INDENT_OUTPUT).build()
+
         val mergedNode = inputFiles
             .get().map { it.asFile }
             .filterNot { it == outputFile.get().asFile }
-            .map { objectMapper.readTree(it) }
-            .reduce { acc, node -> objectMapper.updateValue(acc, node) }
-        objectMapper.writeValue(outputFile.get().asFile, mergedNode)
+            .map { yamlMapper.readTree(it) }
+            .reduce { acc, node -> yamlMapper.updateValue(acc, node) }
+
+        yamlMapper.writeValue(outputFile.get().asFile, mergedNode)
+
+        if (outputFileJson.isPresent) {
+            jsonMapper.writeValue(outputFileJson.get().asFile, mergedNode)
+        }
     }
 }
